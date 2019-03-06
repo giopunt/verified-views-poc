47af0d5c7012bde2047c7fb8aa709ad4a54ecb91
giopunt <puntilgiovanni@gmail.com>
2019-03-06 23:44:13 +0000

    Maked images and title clickable

--- src/components/episode.js
+++ src/components/episode.js
@@ -4,17 +4,38 @@ import styles from "./episode.module.css";
 
 const Episode = ({ date, excerpt, image, title, url }) => (
   <div className={styles.episode}>
-    <h3>{title}</h3>
+    <h3>
+      <a
+        href={url}
+        className={styles.clickable}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {title}
+      </a>
+    </h3>
     <span className={styles.releaseDate}>Released {date}</span>
     <div className={styles.components}>
-      <img
-        src={`https://giopunt.github.io/verified-views-poc/${image}`}
-        alt="In this episode image"
-        className={styles.mainImage}
-      />
+      <a
+        href={url}
+        className={styles.clickable}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={`https://giopunt.github.io/verified-views-poc/${image}`}
+          alt="In this episode image"
+          className={styles.mainImage}
+        />
+      </a>
       <div className={styles.excerpt}>
         <p>{excerpt}</p>
-        <a className={styles.listenNow} href={url}>
+        <a
+          className={styles.listenNow}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <span className={styles.playIcon}>►</span> Listen Now
         </a>
       </div>
